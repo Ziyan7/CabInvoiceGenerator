dce0d7e05b5f934b332cf548ef22d4d849713ad4
Your Name <you@example.com>
2021-09-28 23:09:01 +0530

    Added : Enhaced Invoice with Average Fare per ride

--- src/main/java/userRegistration/TDD/CabInvoice.java
+++ src/main/java/userRegistration/TDD/CabInvoice.java
@@ -1,21 +1,27 @@
 package userRegistration.TDD;
 
+
 public class CabInvoice 
 {
 	private static final int COST_PER_MINUTE = 1;
 	private double MIN_COST_PER_KILOMETER = 10.0;
 	private static final double MINIMUM_FARE = 5;
 	
+	//calculates fare for one ride
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MIN_COST_PER_KILOMETER + time * COST_PER_MINUTE;	
 		return Math.max(totalFare,MINIMUM_FARE);
 	}
 	
-	public double calculateFare(RidesFare[] rides) {
+	//method calculates total fares and count total rides
+	public InvoiceSummary calculateFare(RidesFare[] rides) {
 		double totalFare = 0;
+		int totalRides = 0;
 		for(RidesFare ride:rides) {
+			totalRides++;
 			totalFare += this.calculateFare(ride.distance,ride.time);
 		}
-		return totalFare;
+		
+		return new InvoiceSummary(totalRides,totalFare);
 	}
 }
