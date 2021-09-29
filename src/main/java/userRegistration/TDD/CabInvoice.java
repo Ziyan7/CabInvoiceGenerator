1ae82be9b7f4cd43f0e95f7489945eaf0f8b46c0
Your Name <you@example.com>
2021-09-29 21:51:37 +0530

    Added : 2 categories that is normal and premium

--- src/main/java/userRegistration/TDD/CabInvoice.java
+++ src/main/java/userRegistration/TDD/CabInvoice.java
@@ -1,27 +1,40 @@
 package userRegistration.TDD;
 
 
+
 public class CabInvoice 
 {
+	//normal ride
 	private static final int COST_PER_MINUTE = 1;
 	private double MIN_COST_PER_KILOMETER = 10.0;
 	private static final double MINIMUM_FARE = 5;
 	
-	//calculates fare for one ride
-	public double calculateFare(double distance, int time) {
-		double totalFare = distance * MIN_COST_PER_KILOMETER + time * COST_PER_MINUTE;	
-		return Math.max(totalFare,MINIMUM_FARE);
+	//premium ride
+	private static final int COST_PER_MINUTE_PREMIUM = 2;
+	private static final double MIN_COST_PER_KILOMETER_PREMIUM = 15.0;
+	private static final double MINIMUM_FARE_PREMIUM = 20;
+	
+	
+	// calculates fare for a ride 
+	public double calculateFare(String category,double distance, int time) {
+		if(category.equals("premium")) {
+			double totalFare = distance * MIN_COST_PER_KILOMETER_PREMIUM + time * COST_PER_MINUTE_PREMIUM;
+			return Math.max(totalFare,MINIMUM_FARE_PREMIUM);
+		}else {
+			double totalFare = distance * MIN_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+			return Math.max(totalFare,MINIMUM_FARE);
+		}
 	}
 	
+	
 	//method calculates total fares and count total rides
-	public InvoiceSummary calculateFare(RidesFare[] rides) {
+	public InvoiceSummary calculateFare(String category,RidesFare[] rides) {
 		double totalFare = 0;
 		int totalRides = 0;
 		for(RidesFare ride:rides) {
 			totalRides++;
-			totalFare += this.calculateFare(ride.distance,ride.time);
+			totalFare += this.calculateFare(category,ride.distance,ride.time);
 		}
-		
 		return new InvoiceSummary(totalRides,totalFare);
 	}
 }
--- src/test/java/userRegistration/TDD/CabInvoiceTest.java
+++ src/test/java/userRegistration/TDD/CabInvoiceTest.java
@@ -19,14 +19,14 @@ public class CabInvoiceTest
 		
 		double distance = 2.0;
 		int time = 5;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		double fare = invoiceGenerator.calculateFare("normal",distance, time);
 		Assert.assertEquals(25, fare, 0.0);
 	}
     
     @Test
     public void givenDistanceAndTimeForMultipleRidesShouldReturnAggregate() {
     	RidesFare[] rides = { new RidesFare(5.0,5), new RidesFare(2.0,2)};
-    	InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+    	InvoiceSummary summary = invoiceGenerator.calculateFare("normal",rides);
     	InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,77.0);
     	Assert.assertEquals(expectedInvoiceSummary,summary);
     }
@@ -41,12 +41,28 @@ public class CabInvoiceTest
 		rideRepository.put(2, ridesOfUser2);
 		rideRepository.put(3, ridesOfUser3);
 		
-		InvoiceSummary summary = invoiceGenerator.calculateFare(rideRepository.get(1));
+		InvoiceSummary summary = invoiceGenerator.calculateFare("normal",rideRepository.get(1));
     	InvoiceSummary expctdInvoiceSummary = new InvoiceSummary(2,77.0);
     	Assert.assertEquals(expctdInvoiceSummary,summary);
-    	InvoiceSummary summary1 = invoiceGenerator.calculateFare(rideRepository.get(2));
+    	InvoiceSummary summary1 = invoiceGenerator.calculateFare("normal",rideRepository.get(2));
     	InvoiceSummary expctdInvoiceSummary1 = new InvoiceSummary(2,115.0);
     	Assert.assertEquals(expctdInvoiceSummary1,summary1);
     }
+    
+    @Test
+	public void givenUserIdShouldReturnListOfRidesAndSummaryInvoiceForPremium() {
+    	RidesFare[] ridesOfUser1 = { new RidesFare(5.0,5), new RidesFare(2.0,3)};
+    	RidesFare[] ridesOfUser2 = { new RidesFare(3.0,6), new RidesFare(7.0,9)};
+    	RidesFare[] ridesOfUser3 = { new RidesFare(2.0,7), new RidesFare(4.0,8)};
+    	
+    	Map<Integer,RidesFare[]> rideRepository = new HashMap<>();
+		rideRepository.put(1, ridesOfUser1);
+		rideRepository.put(2, ridesOfUser2);
+		rideRepository.put(3, ridesOfUser3);
+		
+		InvoiceSummary summary = invoiceGenerator.calculateFare("premium",rideRepository.get(1));
+		InvoiceSummary expctdInvoiceSummary = new InvoiceSummary(2,121.0);
+    	Assert.assertEquals(expctdInvoiceSummary,summary);
+	}
     
 }
